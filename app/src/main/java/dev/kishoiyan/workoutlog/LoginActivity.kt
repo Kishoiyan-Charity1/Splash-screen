fceea10a0d57e9d8ce2492ee8ced0ccbab6fdeef
Kishoiyan-Charity1 <kishoiyancharity@gmail.com>
2022-05-24 21:04:15 +0300

    first commit

---
+++ app/src/main/java/dev/kishoiyan/workoutlog/LoginActivity.kt
@@ -0,0 +1,11 @@
+package dev.kishoiyan.workoutlog
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        val intent = (createPackageContext:this, LoginActivity::class)
+    }
+}
